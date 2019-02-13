6d3886ff99dd55576519ff938953a020c933c1c5
owahby <47379737+owahby@users.noreply.github.com>
2019-02-12 23:22:59 -0800

    Add files via upload

---
+++ PE0518A.java
@@ -0,0 +1,16 @@
+public class PE0518A
+{
+  public static void main(String[] args)
+  {
+    final int n = 6;
+
+    for(int i=1; i<=n; i++)
+    {
+
+      for(int j=1; j<=i; j++)
+        System.out.print(j + " ");
+
+      System.out.println();
+    }
+  }
+}
